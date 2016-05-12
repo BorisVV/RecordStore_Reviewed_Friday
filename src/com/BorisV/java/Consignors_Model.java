7b20c91d94fc4baaf4b5e6d02fc6f0b2d67eca89
Boris <bcarl@gmail.com>
2016-05-11 20:52:18 -0500

    Class Consignors_Model Class Record_Store_Data_Model

---
+++ src/com/BorisV/java/Consignors_Model.java
@@ -0,0 +1,98 @@
+package com.BorisV.java;
+
+
+import javax.swing.table.AbstractTableModel;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Consignors_Model extends AbstractTableModel {
+
+    private int row_CountCon = 0;
+    private int col_CountCon = 0;
+    ResultSet resultSetCon;
+
+    Consignors_Model(ResultSet rs) {
+        this.resultSetCon = rs;
+        sepUp();
+    }
+
+    private void sepUp() {
+        count_Rows();
+
+        try {
+            col_CountCon = resultSetCon.getMetaData().getColumnCount();
+
+        } catch (SQLException e) {
+            System.out.println("Error counting columns " + e);
+        }
+
+    }
+
+
+
+
+    @Override
+    public int getRowCount() {
+         count_Rows();
+        return row_CountCon;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return col_CountCon;
+
+    }
+
+    @Override
+    public Object getValueAt(int rowConsig, int colConsig) {
+        try {
+            resultSetCon.absolute(rowConsig + 1);
+            Object conS = resultSetCon.getObject(colConsig + 1);
+            return conS.toString();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            return e.toString();
+        }
+    }
+
+
+    public void count_Rows() {
+        row_CountCon = 0;
+        //Move coursor to the start of the first row
+        try {
+            resultSetCon.beforeFirst();
+            //moves cursor forward one row
+            while ((resultSetCon.next())) {
+                row_CountCon++;
+            }
+            resultSetCon.beforeFirst();
+        } catch (SQLException e) {
+            System.out.println("Error counting rows " + e);
+        }
+    }
+
+
+
+
+    @Override
+    public String getColumnName(int colName){
+        try {
+            return resultSetCon.getMetaData().getColumnName(colName + 1);
+        } catch (SQLException se) {
+            System.out.println("Error fetching column name" + se);
+            return "?";
+        }
+    }
+
+    public void update_ResultSet(ResultSet rsConsignors) {
+        resultSetCon = rsConsignors;
+        sepUp();
+
+    }
+
+
+
+}
+
---
+++ src/com/BorisV/java/Record_Store_Data_Model.java
@@ -0,0 +1,129 @@
+package com.BorisV.java;
+
+import javax.swing.table.AbstractTableModel;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Record_Store_Data_Model  extends AbstractTableModel {
+
+    //add row and  columns
+    private int col_CountRec = 0;
+    private int row_CountRec = 0;
+    private ResultSet resultSetRec;
+
+    Record_Store_Data_Model(ResultSet rs) {
+        this.resultSetRec = rs;
+        sepUp();
+    }
+
+    private void sepUp() {
+        count_RowsRec();
+
+        try {
+            col_CountRec = resultSetRec.getMetaData().getColumnCount();
+
+        } catch (SQLException e) {
+            System.out.println("Error counting columns " + e);
+        }
+
+    }
+
+    public void update_ResultSet(ResultSet new_ResultSet) {
+        resultSetRec = new_ResultSet;
+        sepUp();
+    }
+
+    public boolean deleteRow(int row){
+        try {
+            resultSetRec.absolute(row + 1);
+            resultSetRec.deleteRow();
+            fireTableDataChanged();
+            return true;
+        }catch (SQLException se) {
+            System.out.println("Delete row error " + se);
+            return false;
+        }
+    }
+
+    public void count_RowsRec() {
+        row_CountRec = 0;
+        try {
+            resultSetRec.beforeFirst();
+            while ((resultSetRec.next())) {
+                row_CountRec++;
+            }
+            resultSetRec.beforeFirst();
+        } catch (SQLException e) {
+            System.out.println("Error counting record rows " + e);
+        }
+    }
+
+
+
+
+    @Override
+    public int getRowCount() {
+        count_RowsRec();
+        return row_CountRec;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return col_CountRec;
+    }
+
+    @Override
+    public Object getValueAt(int rowRec, int colRec) {
+        try {
+            resultSetRec.absolute(rowRec+1);
+            Object rec = resultSetRec.getObject(colRec+1);
+            return rec.toString();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error Objet getValue");
+
+            return e.toString();
+        }
+    }
+
+
+
+
+    public boolean insertRow(String artist, String title, int consignor, double price) {
+
+        try {
+            //Move to insert row, insert the appropriate data in each column, insert the row, move cursor back to where it was before we started
+            resultSetRec.moveToInsertRow();
+            resultSetRec.updateString(Record_Store_Data_Base.ARTIST_NAME, artist);
+            resultSetRec.updateString(Record_Store_Data_Base.ALBUM_NAME, title);
+            resultSetRec.updateInt(Record_Store_Data_Base.CONSIGNOR_ID, consignor);
+            resultSetRec.updateDouble(Record_Store_Data_Base.PRICE, price);
+            resultSetRec.insertRow();
+            resultSetRec.moveToCurrentRow();
+            fireTableDataChanged();
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error adding row");
+            return false;
+        }
+    }
+
+    @Override
+    public String getColumnName(int col){
+        try {
+            return resultSetRec.getMetaData().getColumnName(col + 1);
+        } catch (SQLException se) {
+            System.out.println("Error fetching column names" + se);
+            return "?";
+        }
+    }
+
+}
+
+
+
+
+
+
